cee7d1241ea747e9a35a5df6c3d652c97f5272c3
Slyn4x <remlouplay3@gmail.com>
2024-06-05 11:08:53 +0200

    client/server merge  table de routage marche avec r1,r2,r3

--- src/rip.rs
+++ src/rip.rs
@@ -1,11 +1,10 @@
 use std::net::{UdpSocket, Ipv4Addr};
 use std::thread;
-use std::time::Duration;
 use crate::router::InterfaceWrapper;
 use serde::{Serialize, Deserialize};
-use core::net::SocketAddr;
-use core::net::IpAddr;
 use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
+use std::net::IpAddr;
 
 const RIP_PORT: u16 = 8080;
 
@@ -18,28 +17,48 @@ pub struct Router {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Route {
     pub network: Ipv4Addr,
-    pub mask: u8,
-    pub next_hop: Ipv4Addr,
+    pub mask: Ipv4Addr,
+    pub next_hop: Option<Ipv4Addr>,
     pub metric: u8,
+    pub exit_interface: Ipv4Addr,
+}
+
+// Fonction pour déterminer l'interface de sortie en fonction de l'adresse IP de destination
+fn get_exit_interface(config: &[InterfaceWrapper], dest_ip: Ipv4Addr) -> Option<Ipv4Addr> {
+    for interface in config {
+        let ip = interface.interface.ip.parse::<Ipv4Addr>().expect("Failed to parse IP address");
+        let mask = mask_to_ip(interface.interface.mask);
+        let network = get_network(ip, mask);
+        if network == get_network(dest_ip, mask) {
+            return Some(ip);
+        }
+    }
+    None
+}
+
+// Fonction pour initialiser les routes à partir des fichiers YAML
+fn initialize_routes(config: &[InterfaceWrapper]) -> Vec<Route> {
+    let mut routes = Vec::new();
+    for interface in config {
+        let ip = interface.interface.ip.parse::<Ipv4Addr>().expect("Failed to parse IP address");
+        let mask = mask_to_ip(interface.interface.mask);
+        let network = get_network(ip, mask);
+        let route = Route {
+            network,
+            mask,
+            next_hop: None,
+            metric: 1,
+            exit_interface: ip,
+        };
+        routes.push(route);
+    }
+    routes
 }
 
 impl Router {
     pub fn new(config: Vec<InterfaceWrapper>) -> Self {
-        let routing_table = Arc::new(Mutex::new(Vec::new()));
+        let routing_table = Arc::new(Mutex::new(initialize_routes(&config)));
         let send_socket = None;
-
-        for interface in &config {
-            let ip = interface.interface.ip.parse::<Ipv4Addr>().expect("Failed to parse IP address");
-            let mask = interface.interface.mask;
-            let network = get_network(ip, mask);
-            let route = Route {
-                network,
-                mask,
-                next_hop: ip,
-                metric: 1,
-            };
-            routing_table.lock().unwrap().push(route);
-        }
 
         Router { config, routing_table, send_socket }
     }
@@ -62,19 +81,20 @@ impl Router {
                 let (amt, _src) = recv_socket.recv_from(&mut buf).expect("Failed to receive data");
                 let received_data = &buf[..amt];
                 let mut routing_table = routing_table.lock().unwrap();
-                update_routing_table(&mut routing_table, received_data);
+                update_routing_table(&mut routing_table, received_data, &config);
             }
         });
     }
 
-    pub fn send_update(&self, dest_ip: Ipv4Addr) {
+    pub fn send_update(&self, dest_ip: Ipv4Addr, exit_interface_ip: Ipv4Addr) {
         let send_socket = self.send_socket.as_ref().expect("Send socket not initialized");
         let dest = SocketAddr::new(IpAddr::V4(dest_ip), RIP_PORT);
     
         let routing_table = self.routing_table.lock().unwrap().clone();
-        let data = bincode::serialize(&routing_table).expect("Failed to serialize data");
+        let data = (exit_interface_ip, routing_table);
+        let serialized_data = bincode::serialize(&data).expect("Failed to serialize data");
     
-        send_socket.send_to(&data, &dest).expect("Failed to send data");
+        send_socket.send_to(&serialized_data, &dest).expect("Failed to send data");
     }
 
     pub fn get_routing_table(&self) -> Vec<Route> {
@@ -83,29 +103,40 @@ impl Router {
     }
 }
 
-fn update_routing_table(routing_table: &mut Vec<Route>, data: &[u8]) {
-    let received_routes: Vec<Route> = bincode::deserialize(data).expect("Failed to deserialize data");
+fn update_routing_table(routing_table: &mut Vec<Route>, data: &[u8], config: &[InterfaceWrapper]) {
+    let (exit_interface_ip, received_routes): (Ipv4Addr, Vec<Route>) = bincode::deserialize(data).expect("Failed to deserialize data");
 
-    for route in received_routes {
+    for mut route in received_routes {
+        route.next_hop = Some(exit_interface_ip); // Mise à jour du next_hop
+        if let Some(exit_interface) = get_exit_interface(config, exit_interface_ip) {
+            route.exit_interface = exit_interface; // Mise à jour de l'interface de sortie
+        }
         add_or_update_route(routing_table, route);
     }
 }
 
-fn add_or_update_route(routing_table: &mut Vec<Route>, route: Route) {
+fn add_or_update_route(routing_table: &mut Vec<Route>, mut route: Route) {
     let existing_route = routing_table.iter().find(|r| r.network == route.network && r.mask == route.mask);
     if let Some(existing_route) = existing_route {
         if existing_route.metric > route.metric {
             routing_table.retain(|r| r.network != route.network || r.mask != route.mask);
+            route.metric += 1;
             routing_table.push(route);
         }
     } else {
+        route.metric += 1;
         routing_table.push(route);
     }
 }
 
-fn get_network(ip: Ipv4Addr, mask: u8) -> Ipv4Addr {
+fn get_network(ip: Ipv4Addr, mask: Ipv4Addr) -> Ipv4Addr {
     let ip_int = u32::from(ip);
-    let mask_int = !(0xFFFFFFFFu32 >> mask);
+    let mask_int = u32::from(mask);
     let network_int = ip_int & mask_int;
     Ipv4Addr::from(network_int)
+}
+
+fn mask_to_ip(mask: u8) -> Ipv4Addr {
+    let mask_int = !(0xFFFFFFFFu32 >> mask);
+    Ipv4Addr::from(mask_int)
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,23 +1,23 @@
 use prettytable::{Table, Row, Cell, row, cell};
 
 use crate::router::InterfaceWrapper;
-use core::net::Ipv4Addr;
+use std::net::Ipv4Addr;
 use crate::rip::Router;
 use std::time::Duration;
 use crate::rip::Route;
 
 fn print_routing_table(router_name: &str, routing_table: &[Route]) {
     let mut table = Table::new();
-    table.add_row(row![cell!("Network"), cell!("Mask"), cell!("Next Hop"), cell!("Metric")]);
+    table.add_row(row![cell!("Network"), cell!("Mask"), cell!("Next Hop"), cell!("Interface"), cell!("Metric")]);
 
     for route in routing_table {
-
         let network_cell = Cell::new(&route.network.to_string());
         let mask_cell = Cell::new(&route.mask.to_string());
-        let next_hop_cell = Cell::new(&route.next_hop.to_string());
+        let next_hop_cell = Cell::new(&route.next_hop.map_or("None".to_string(), |ip| ip.to_string()));
+        let exit_interface = Cell::new(&route.exit_interface.to_string());
         let metric_cell = Cell::new(&route.metric.to_string());
 
-        let row = Row::new(vec![network_cell, mask_cell, next_hop_cell, metric_cell]);
+        let row = Row::new(vec![network_cell, mask_cell, next_hop_cell, exit_interface, metric_cell]);
 
         table.add_row(row);
     }
@@ -30,9 +30,11 @@ fn print_routing_table(router_name: &str, routing_table: &[Route]) {
 fn test_rip_simulation() {
     let router1_config = InterfaceWrapper::from_yaml("./config/routeur-r1.yaml");
     let router2_config = InterfaceWrapper::from_yaml("./config/routeur-r2.yaml");
+    let router3_config = InterfaceWrapper::from_yaml("./config/routeur-r3.yaml");
 
     let (tx1, rx1) = std::sync::mpsc::channel();
     let (tx2, rx2) = std::sync::mpsc::channel();
+    let (tx3, rx3) = std::sync::mpsc::channel();
 
     let ip1 = Ipv4Addr::new(127, 0, 0, 1);
     let handle1 = std::thread::spawn(move || {
@@ -48,17 +50,39 @@ fn test_rip_simulation() {
         tx2.send(router2).unwrap();
     });
 
+    let ip3 = Ipv4Addr::new(127, 0, 0, 3);
+    let handle3 = std::thread::spawn(move || {
+        let mut router3 = Router::new(router3_config);
+        router3.start(ip3);
+        tx3.send(router3).unwrap();
+    });
+
     let mut router1 = rx1.recv().unwrap();
     let mut router2 = rx2.recv().unwrap();
+    let mut router3 = rx3.recv().unwrap();
 
-    router1.send_update(ip2);
-    router2.send_update(ip1);
+    let ip1_1 = Ipv4Addr::new(10, 1, 1, 1);
+    let ip2_1 = Ipv4Addr::new(10, 1, 1, 2);
+    let ip2_2 = Ipv4Addr::new(10, 1, 2, 1);
+    let ip3_1 = Ipv4Addr::new(10, 1, 2, 2);
+
+    router1.send_update(ip2, ip1_1);
+    std::thread::sleep(Duration::from_micros(500));
+    router2.send_update(ip1, ip2_1);
+    std::thread::sleep(Duration::from_micros(500));
+    router3.send_update(ip2, ip3_1);
+    std::thread::sleep(Duration::from_micros(500));
+    router2.send_update(ip1, ip2_1);
+    std::thread::sleep(Duration::from_micros(500));
+    router2.send_update(ip3, ip2_2);
 
     std::thread::sleep(Duration::from_secs(2));
 
     print_routing_table("Router1", &router1.get_routing_table());
     print_routing_table("Router2", &router2.get_routing_table());
+    print_routing_table("Router3", &router3.get_routing_table());
 
     handle1.join().expect("Router 1 failed");
     handle2.join().expect("Router 2 failed");
+    handle3.join().expect("Router 3 failed");
 }
